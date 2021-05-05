c642b4812063c5c36489b80eb42b6aecf2f258a9
jomy-kk <joaomiguelsaraiva@tecnico.ulisboa.pt>
2021-05-05 15:34:56 +0100

    Time domain labels

--- src/feature_extraction/TimeFeaturesCalculator.py
+++ src/feature_extraction/TimeFeaturesCalculator.py
@@ -5,37 +5,47 @@ from feature_extraction.HRVFeaturesCalculator import HRVFeaturesCalculator
 
 
 class TimeFeaturesCalculator(HRVFeaturesCalculator):
-
+    
     def __init__(self, nni_signal, sampling_frequency):
         super().__init__('linear', nni_signal)
+        
         self.sf = sampling_frequency
+    
+    # Labels
+    nn50_label  = 'NN50'
+    pnn50_label = 'PPN50'
+    sdnn_label  = 'Std Dev NNI'
+    rmssd_label = 'Root Mean Std Dev NNI'
+    mean_label  = 'Mean NNI'
+    var_label   = 'Variance NNI'
+    
 
     def get_nn50(self):
         if not hasattr(self, 'nn50'):
             self.nn50 = len(np.argwhere(abs(np.diff(self.nni)) > 0.05 * self.sf))
-        return FeatureSequence(self.nn50, 'NN50')
+        return self.nn50
 
     def get_pnn50(self):
         if not hasattr(self, 'pnn50'):
             self.pnn50 = self.get_nn50() / len(self.nni)
-        return FeatureSequence(self.pnn50, 'PPN50')
+        return self.pnn50
 
     def get_sdnn(self):
         if not hasattr(self, 'sdnn'):
             self.sdnn = self.nni.std()
-        return FeatureSequence(self.sdnn, 'Std Dev NNI')
+        return self.sdnn
 
     def get_rmssd(self):
         if not hasattr(self, 'rmssd'):
             self.rmssd = ((np.diff(self.nni) ** 2).mean()) ** 0.5
-        return FeatureSequence(self.rmssd, 'Root Mean Std Dev NNI')
+        return self.rmssd
 
     def get_mean(self):
         if not hasattr(self, 'mean'):
             self.mean = self.nni.mean()
-        return FeatureSequence(self.mean, 'Mean NNI')
+        return self.mean
 
     def get_var(self):
         if not hasattr(self, 'var'):
             self.var = self.nni.var()
-        return FeatureSequence(self.var, 'Variance NNI')
+        return self.var
