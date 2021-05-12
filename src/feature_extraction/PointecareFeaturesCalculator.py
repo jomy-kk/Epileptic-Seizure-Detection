62717d76234d5f865384981815bb70bd609a8127
deboraal-tech <83726667+deboraal-tech@users.noreply.github.com>
2021-05-12 21:57:39 +0100

    Added S feature to Poincare plot

--- src/feature_extraction/PointecareFeaturesCalculator.py
+++ src/feature_extraction/PointecareFeaturesCalculator.py
@@ -10,7 +10,7 @@ class PointecareFeaturesCalculator(HRVFeaturesCalculator):
         super().__init__('non-linear', nni_signal)
 
     # Labels
-    labels = {'sd1': 'SD1', 'sd2': 'SD2', 'csi': 'CSI (SD2/SD1)', 'csv': 'CSV'}
+    labels = {'sd1': 'SD1', 'sd2': 'SD2', 'csi': 'CSI (SD2/SD1)', 'csv': 'CSV', 's': 'S: area of ellipse fitted to Poincaré plot'}
 
     def get_sd1(self):
         if not hasattr(self, 'sd1'):
@@ -33,3 +33,8 @@ class PointecareFeaturesCalculator(HRVFeaturesCalculator):
         if not hasattr(self, 'csv'):
             self.csv = math.log10(self.get_sd1() * self.get_sd2())
         return self.csv
+
+    def get_s(self):
+        if not hasattr(self, 's'):
+            self.s = math.pi * self.get_sd1() * self.get_sd2()
+        return self.s
